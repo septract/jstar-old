41f8bb46a45bbebc76eee354eac14c347f169780
Justus Mattheisen <jm614@cam.ac.uk>
2009-08-06 22:17:50 +0000

    Added support for relative file names in import statements.

--- src/prover/load_logic.ml
+++ src/prover/load_logic.ml
@@ -5,13 +5,14 @@ open Global_types
 
 let load_logic dirs filename = 
   (* Converts a list of rules and imports into a logic *)
-  let rec rule_list_to_logic rl (sl,rm)  =
+  let rec rule_list_to_logic filename rl (sl,rm) =
+    let rel_dir = (Filename.dirname filename) in
     let rec rule_list_to_logic_inner rl (sl,rm) =
       match rl with
 	[] -> sl,rm
       | r :: rl -> let (sl,rm) = rule_list_to_logic_inner rl (sl,rm) in 
 	match r with
-	|	Import(file) -> load_logic_inner file (sl,rm)
+	| Import(file) -> load_logic_inner (rel_dir::dirs) file (sl,rm)
 	| SeqRule(r) -> (r::sl,rm)
 	| RewriteRule(r) -> 
 	    (match r with 
@@ -20,7 +21,7 @@ let load_logic dirs filename =
     let sl,rm = rule_list_to_logic_inner rl (sl,rm) in
     sl,rm
 (* Loads a file and all its imports *)
-  and load_logic_inner filename (sl,rm) = 
+  and load_logic_inner dirs filename (sl,rm) = 
     let filename = 
       try 
 	System.find_file_from_dirs dirs filename 
@@ -29,8 +30,8 @@ let load_logic dirs filename =
     let l = string_of_file filename in 
     if !(Debug.debug_ref) then Printf.printf "Start parsing logic in %s...\n" filename;
     let rule_list  = Logic_parser.rule_file Logic_lexer.token (Lexing.from_string l) in 
-    let logic = rule_list_to_logic rule_list (sl,rm) in 
+    let logic = rule_list_to_logic filename rule_list (sl,rm) in 
     if !(Debug.debug_ref) then Printf.printf "Parsed %s!\n" filename;
     logic in 
-  let sl,rm = load_logic_inner filename  ([],Rterm.rm_empty) in 
+  let sl,rm = load_logic_inner dirs filename ([],Rterm.rm_empty) in 
   sl,rm,default_pure_prover
