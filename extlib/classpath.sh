4cef587d25a76b916f1e42d778a1459fbc316d62
Matthew Parkinson <mjp41@cl.cam.ac.uk>
2010-03-26 08:50:57 +0000

    Moved external libraries outside of trunk.  Removed my old branch for the new prover.

--- extlib/classpath.sh
+++
@@ -1,7 +0,0 @@
-export PWD=`pwd`
-CLASSPATHSOOT=""
-for i in *.jar; do CLASSPATHSOOT=$CLASSPATHSOOT:$PWD/$i; done
-export CLASSPATHSOOT=$CLASSPATHSOOT
-export CLASSPATH=$CLASSPATH:$CLASSPATHSOOT:/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar:.
-
-
--- extlib/set_classpath_cygwin.sh
+++
@@ -1,7 +0,0 @@
-export PWD=`cygpath -aw .`
-CLASSPATHSOOT=""
-for i in *.jar; do CLASSPATHSOOT=$CLASSPATHSOOT";"$PWD\\$i; done
-export CLASSPATHSOOT=$CLASSPATHSOOT
-export CLASSPATH=$CLASSPATH";"$CLASSPATHSOOT";"/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar";".
-
-
