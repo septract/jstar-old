2becc7416864bdcda10bb1a911df9e30e9800e33
Mike Dodds <md466@cl.cam.ac.uk>
2010-04-14 15:20:30 +0100

    Added environment setting script and instructions for native building on Win32 (thanks Matko!)

---
+++ setjstarenv.bat
@@ -0,0 +1,6 @@
+@echo off
+set PATH=%PATH%;%CD%\bin
+
+set JSTAR_LOGIC_LIBRARY=%CD%\library\logic
+set JSTAR_ABS_LIBRARY=%CD%\library\abstraction
+set JSTAR_SPECS_LIBRARY=%CD%\library\specifications
