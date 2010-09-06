367e368fb579f93afc8ea9a9ada7f8255e46186c
Radu Grigore <radugrigore@gmail.com>
2010-09-06 09:46:38 +0100

    Old changes.

--- src/utils/debug.ml
+++ src/utils/debug.ml
@@ -77,7 +77,7 @@ let log_prove = 1 lsl 3
 let log_exec = 1 lsl 4
 let log_logic = 1 lsl 5
 
-let log_active = 0
+let log_active = log_prove
   (* -1 means all, 0 means one, in general use lor *)
 
 let log x = log_active land x != 0
