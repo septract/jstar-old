4a92d3101827a309d259128ae9d4e4975921889a
Matko Botincan <mb741@cam.ac.uk>
2010-12-17 14:02:53 +0000

    Merging abductfront into symbfront.

--- src/symbfront/symbfront.ml
+++ src/symbfront/symbfront.ml
@@ -23,6 +23,7 @@ open Psyntax
 
 let question_file_name = ref "";;
 let logic_file_name = ref "";;
+let abduct_logic_file_name = ref "";;
 let absrules_file_name = ref "";;
 
 let set_question_file_name fn =
@@ -32,13 +33,13 @@ let set_question_file_name fn =
 let arg_list = Config.args_default @ 
   [ ("-f", Arg.String set_question_file_name, "question file name" );
   ("-l", Arg.Set_string logic_file_name, "logic file name" );
+  ("-al", Arg.Set_string abduct_logic_file_name, "abduction logic file name" );
   ("-a", Arg.Set_string absrules_file_name, "abstraction rules file name" );
 ]
 
 
 let main () : unit = 
-  let usage_msg=
-"Usage: -l <logic_file_name>  -a <abstraction_file_name>  -f <question_file_name>" in 
+  let usage_msg = "Usage: -l <logic_file_name>  [-al <abduct_logic_file_name>]  -a <abstraction_file_name>  -f <question_file_name>" in 
   Arg.parse arg_list (fun s ->()) usage_msg;
 
   if !question_file_name="" then 
@@ -54,21 +55,35 @@ let main () : unit =
     let lo = {empty_logic with seq_rules = l1; rw_rules = l2; consdecl = cn} in
     let l1,l2,cn = Load_logic.load_logic  (System.getenv_dirlist "JSTAR_LOGIC_LIBRARY") !absrules_file_name in 
     let abs_rules = {empty_logic with seq_rules = l1; rw_rules = l2; consdecl = cn} in
-    let question_list = 
-      System.parse_file 
-          Jparser.symb_question_file 
-          Jlexer.token 
-          !question_file_name 
-          "Question" in
-    List.iter (
-    fun question ->
-      match question with 
-       | Specification(mname,spec,core)  ->
-          Format.printf "Method: %s\nSpec: %a"  mname  Spec.spec2str spec; 
-       	  let stmts_core = map Cfg_core.mk_node core in 
-          if Symexec.verify mname stmts_core spec lo abs_rules then
-          Format.printf "\nGood specification!\n\n" else Format.printf "\nBad specification!\n\n" 
-    ) question_list
+    
+    if !abduct_logic_file_name="" then
+      let question_list = System.parse_file Jparser.symb_question_file Jlexer.token !question_file_name "Question" in
+      List.iter (
+      fun question ->
+        match question with 
+         | Specification(mname,spec,core)  ->
+            Format.printf "Method: %s\nSpec: %a"  mname  Spec.spec2str spec; 
+            let stmts_core = map Cfg_core.mk_node core in 
+            if Symexec.verify mname stmts_core spec lo abs_rules then
+            Format.printf "\nGood specification!\n\n" else Format.printf "\nBad specification!\n\n" 
+      ) question_list     
+    else
+      let l1,l2,cn = (load_logic (System.getenv_dirlist "JSTAR_LOGIC_LIBRARY") !abduct_logic_file_name) in 
+      let abduct_lo = {empty_logic with seq_rules=l1; rw_rules=l2; consdecl=cn} in
+      let question_list = System.parse_file Jparser.symb_question_file Jlexer.token !question_file_name "Question" in
+      List.iter (
+      fun question ->
+        match question with 
+         | Specification(mname,spec,core)  ->
+            Format.printf "\nMethod: %s\nSpec: %a"  mname  Spec.spec2str spec; 
+            let stmts_core = map Cfg_core.mk_node core in 
+            let specs = Symexec.bi_abduct mname stmts_core spec lo abduct_lo abs_rules in
+            Format.printf "\nDiscovered specs:\n";
+            List.iter (fun (spec_pre, spec_post) ->
+              Format.printf "@\npre:@\n    %a@." Sepprover.string_inner_form spec_pre;
+              Format.printf "@\npost:@\n    %a@." Sepprover.string_inner_form spec_post;)
+              specs
+      ) question_list
 
 
 let _ = main ()
