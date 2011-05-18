73287b757ca7094d21437a49144e26afdd005c35
Radu Grigore <radugrigore@gmail.com>
2011-05-18 19:49:06 +0100

    Nice error for time limit exceeded in tests.

---
+++ unit_tests/jimple_tests/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -z "$TIMELIMIT" ]; then
+  TIMELIMIT=5
+fi
+echo -n .
+( ulimit -t $TIMELIMIT; $1/bin/jstar -f $2.jimple ) 2> /dev/null
+EC=$?
+case $EC in
+  137)
+    echo
+    echo "Time limit (${TIMELIMIT}s) exceeded. Try"
+    echo "  TIMELIMIT=${TIMELIMIT}0 make test"
+    echo "(or some other number >$TIMELIMIT)."
+    ;;
+  0)
+    ;;
+  *)
+    echo 
+    echo "Failed $2 in $(pwd) (errorcode $EC)"
+    ;;
+esac
+
